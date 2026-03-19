44de8526540843065acb0b6c751851c1f0aa86cd
bryanwessantana <bryanwessantana@gmail.com>
2026-03-19 00:48:33 -0300

    Fix TS types and middleware edge compatibility

--- nextjs-dashboard/middleware.ts
+++ nextjs-dashboard/middleware.ts
@@ -1,7 +1,8 @@
-import { auth } from "./auth";
- 
-export default auth;
- 
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+
+export default NextAuth(authConfig).auth;
+
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
