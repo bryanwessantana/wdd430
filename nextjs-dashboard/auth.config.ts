262e98e1a71396b3888e14a5c1b635c4ee3f2286
bryanwessantana <bryanwessantana@gmail.com>
2026-03-19 00:50:53 -0300

    Fixed build error

--- nextjs-dashboard/auth.config.ts
+++ nextjs-dashboard/auth.config.ts
@@ -1,23 +1,21 @@
 import type { NextAuthConfig } from 'next-auth';
- 
+
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
   callbacks: {
-    // Adicionamos a tipagem explícita nos argumentos para resolver o erro de build
-    authorized({ auth, request: { nextUrl } }: { auth: any; request: { nextUrl: any } }) {
+    authorized({ auth, request: { nextUrl } }: any) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return false; // Redireciona usuários não autenticados para o login
+        return false;
       } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
     },
   },
-  providers: [], // Adicione provedores aqui
+  providers: [], 
 } satisfies NextAuthConfig;
